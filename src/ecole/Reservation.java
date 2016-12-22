7eac7f9b5b120f72852dad416c59d2cc2ab6f6b1
TOUKRICHTE <toukrichteupwork@gmail.com>
2016-12-22 23:19:41 +0000

    ss
    test thread

--- src/ecole/Reservation.java
+++ src/ecole/Reservation.java
@@ -1,35 +1,45 @@
 package ecole;
 
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import exceptionClasses.InvalidInputExeption;
 
 public class Reservation extends Thread{
 
 	private int id_reservation;
-	private Date dateDebut;
-	private Date dateFin;
+	private Date dateDebut = new Date();
+	private Date dateFin = new Date();
+	private long duree;
 	private List<Local> listeLocaux = new ArrayList<Local>();
 	private Client client;
 
-	public Reservation() {
-		SimpleDateFormat date = new SimpleDateFormat("YYYY-MM-DD hh:mm:ss");
-		try {
-			dateFin = date.parse("2016-12-22 20:22:50");
-			dateDebut = date.parse("2016-12-22 20:23:00");
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//	public Reservation() {
+//		SimpleDateFormat date = new SimpleDateFormat("YYYY-MM-
+	//DD hh:mm:ss");
+//		try {
+//			dateFin = date.parse("2016-12-22 20:23:10");
+//			dateDebut = date.parse("2016-12-22 20:23:00");
+//		} catch (ParseException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+//	}
 
+
+
+	public long getDuree() {
+		return duree;
 	}
-
-
-
+	public void setDuree(long duree) {
+		duree = dateFin.getTime()-dateDebut.getTime();
+	}
 	public int getId_reservation() {
 		return id_reservation;
 	}
@@ -62,36 +72,69 @@ public class Reservation extends Thread{
 	}
 
 	@Override
-	public synchronized void run() {
+	public void run() {
 
 		Local local;
 		if(Local.isEtat() == false){
 
+			System.out.println(Local.isEtat());
 			local = new Local();
 			local.setId_local(1);
 			listeLocaux.add(local);
 			local.setEtat(true);
-			Client client = new Client();
+			client = new Client();
+
 			try {
 				client.setNom("toto");
-			} catch (InvalidInputExeption e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			try {
-				System.out.println(local.isEtat());
-				Thread.sleep((dateFin.getTime()-dateDebut.getTime()));
+				System.out.println("etat : "+local.isEtat()+", "
+						+ "duration :"+getDuree());
+				Thread.sleep((dateFin.getTime()-dateDebut.
+						getTime()));
 				local.setEtat(false);
+
 				System.out.println(local.isEtat());
-			} catch (InterruptedException e) {
+			} catch (InterruptedException | InvalidInputExeption e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 
 		}
 		else{
-			System.out.println("Desolé salle deja reservée elle sera disponible a la date :"+dateFin);
+			System.out.println("Desolé salle deja reservée "
+					+ "elle sera disponible a la date :"+dateFin);
 		}
 	}
+
+	public boolean reserverLocal(){
+		while(true){
+			SimpleDateFormat date = new SimpleDateFormat("YYYY-"
+					+ "MM-DD hh:mm:ss");
+			Scanner scanner = new Scanner(System.in);
+			System.out.print("entrez la date debut : ");
+			try {
+				dateDebut = date.parse(scanner.nextLine());
+				System.out.println("tapez la date fin : ");
+				dateFin = date.parse(scanner.nextLine());
+			} catch (ParseException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			Reservation reservation1 = new Reservation();
+
+			reservation1.start();
+			try {
+				reservation1.join(reservation1.getDuree());
+				if(Thread.currentThread().isAlive()) Thread.
+				currentThread().interrupt();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+
 
 }
